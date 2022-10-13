8cf368919b2370519ea444fa03a2cfffb50afd88
Greenman727 <cornerx3m@gmail.com>
2022-10-13 11:09:39 -0700

    Helloworld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*First entry
-*Return: Always 0
-*/
-int main(void)
+ *Main - entry
+ *Return: Always 0
+ */
+int main(vid)
 {
 	puts("\programmin is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
