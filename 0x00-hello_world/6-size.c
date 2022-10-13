2bfca62d3869855977ea323660960cc41a2727eb
Greenman727 <cornerx3m@gmail.com>
2022-10-13 14:48:36 -0700

    Helloworld

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 /**
- *main- entry point for the program
+ * Main - Entry point
  *
- *Return: returns zero if no erros otherwise returns non-zero value.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building 
-#include <stdio.h>
-/**
- *main -is the entry point for the program.
- *Return:zero if no error, Non-zero value if errors.
- */
-int main(void)
-{
-	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(longint));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of float: %i bytes(s)\n", sizeof(float));
 	return (0);
 }
