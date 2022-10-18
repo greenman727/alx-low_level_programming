8c4bce852ef4f17c4b4de215874b3063d72bf1a2
Greenman727 <cornerx3m@gmail.com>
2022-10-18 11:40:51 -0700

    nestedloops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+/**
+ * print_alphabet - print the alpha in lower case
+ *
+ */
+void print_alphabet(void)
+{
+	for (j = 'x''; j <= 10; j++)
+
+	{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+
+	_putchar('\n');
+	}
+}
