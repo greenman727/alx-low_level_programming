05158dd3470880b2b5219d51953c83561de002e5
Greenman727 <cornerx3m@gmail.com>
2022-10-18 14:28:37 -0700

    nestedloops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * _isalpha - checks if c is a letter
+ * @c: character to be checked
+ * Return: 1 if c is a letter, 0 if not
+ */
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
