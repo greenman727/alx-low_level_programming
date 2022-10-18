b9585e4cbcd4f25b3b5dbcbdba89980e52576c86
Greenman727 <cornerx3m@gmail.com>
2022-10-18 08:58:35 -0700

    nestedloops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - program to print string
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
