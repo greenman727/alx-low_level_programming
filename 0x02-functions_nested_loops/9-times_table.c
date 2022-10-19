eeabda2d285253763adc37c16e86247f9935ab8f
Greenman727 <cornerx3m@gmail.com>
2022-10-18 18:41:06 -0700

    nestedloops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 
 		for (mult = 1, mult <= 9, mult++)
 		{
-			_putchar(' , ');
+			_putchar(',');
 			_putchar(' ');
 			prod = num * mult;
 
@@ -23,6 +23,6 @@ void times_table(void)
 				_putchar((prod / 10), '0');
 			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
