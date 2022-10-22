7396c4d236778fb0a6eb51f2175860004b9cb876
Greenman727 <cornerx3m@gmail.com>
2022-10-22 16:12:05 -0700

    nestedloops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - print the 9 times_table
+ * times_table - print the 9 times_table, starting with 0
  */
 void times_table(void)
 {
@@ -9,38 +9,32 @@ void times_table(void)
 	int mult;
 	int prod;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
 	{
-		for (mult = 1; mult < 9; mult++)
+		for (mult = 1; mult < 10; mult++)
 		{
 			prod = num * mult;
-		if ((prod / 10) == 0)
-		{
 			if (mult == 0)
 			{
-				_putchar ('0');
-				if (mult != 0)
-				{
-					_putchar (' ');
-					_putchar ((prod % 10) + '0');
-				}
-				if (mult < 9)
-				{
-					_putchar (',');
-					_putchar (' ');
-				}
+				_putchar(prod + '0');
 			}
-			else
-			{
-				_putchar ((prod / 10) + '0');
-				_putchar ((prod % 10 + '0');
-					if (mult < 9)
-					{
-					_putchar (',');
-					_putchar (' ');
-					}
+			
+			if (prod < 10 && mult != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + '0');
 				}
-			}
-			_putchar ('\n');
+				else if (prod >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((prod / 10) +'0');
+					_putchar((prod % 10) +'0');
+				}
+		}
+		_putchar('\n');
+		}
 	}
 }
