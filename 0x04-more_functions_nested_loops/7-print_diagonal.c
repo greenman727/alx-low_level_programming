f8da068b1c7eebc6675a4dc274d99e001b605b9c
Greenman727 <cornerx3m@gmail.com>
2022-10-22 12:26:50 -0700

    morefunctions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a siagonal line
+ * print_diagonal - draws a diagonal line
  * @n: number of '\' to be printed
+ *
+ * Return: void
  */
 void print_diagonal(int n)
 {
 	int x, int y;
 	if (n <= 0)
-		_putchar('\');
+		_putchar('\n');
 
 	for (x = 0; x < n; x++)
 	{
 		for (y = 0; y < x; y++)
+		{
 			_putchar(' ');
+		}
 
 		_putchar('\\');
 		_putchar('\n');
