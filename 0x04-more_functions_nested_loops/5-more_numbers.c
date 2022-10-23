ee59c6469c42175fdbd5499108e4dba7dcafa217
Greenman727 <cornerx3m@gmail.com>
2022-10-23 13:04:17 -0700

    morefunctions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,12 @@ void more_numbers(void)
 {
 	int a, int num;
 
-	for (a = '1'; a <= '10'; a++)
+	for (a = 1; a <= 10; a++)
 	{
-		for (num = '0'; num <= '14'; num++)
+		for (num = 0; num <= 14; num++)
 		{
-			if (num <= '10')
-				_putchar('1');
+			if (num <= 10)
+				_putchar(1);
 				_putchar ((num % 10) + '0');
 		}
 		_putchar('\n');
