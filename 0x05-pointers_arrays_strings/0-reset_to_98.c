4857dfae0de4753b1f08eb531bf941be0a64c272
Greenman727 <cornerx3m@gmail.com>
2022-10-24 04:41:27 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets to another value
+ * Return: void
+ */
+void rese_to_98(int *n)
+{
+	int a = 402;
+	int *n = &a;
+	*n = 98;
+	{
+		_putchar("a = %d\n", *n);
+	}
+return: 0;
+}
+
