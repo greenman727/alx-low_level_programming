fa63c6125ebb6d29149e07671d7cada9bbf5f68d
Greenman727 <cornerx3m@gmail.com>
2022-10-24 18:35:20 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,15 +2,17 @@
 
 /**
  * print_rev - prints a string in reverse
- * @s: string to be printed
+ * @s: the string to be printed
  */
 void print_rev(char *s)
 {
+	int i;
 	int j = 0;
 	
 	while (s(j) != '\0')
 		j++;
+	
 	for (j = j - 1; j >= 1; j --)
-		_putchar(s[j]);
+		_putchar(s[i]);
 	_putchar('\n');
 }
