086d36bb04a48a535664bef9d3a80eb5b0bc5790
Greenman727 <cornerx3m@gmail.com>
2022-11-02 16:20:21 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printrtev_recursion - prints a string, followed by a new line.
+ * _print_rev_recursion - prints a string in reverse
  * @s: string
  * Return: no return.
  */
@@ -9,7 +9,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(*s);
+		_print_rev_recursion *s++;
 	}
 	_putchar('\n');
 }
