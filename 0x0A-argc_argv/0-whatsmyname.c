33a3171e584bff402a347b83ce4125f1962d5fa0
Greenman727 <cornerx3m@gmail.com>
2022-11-04 16:49:59 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print the name of a file
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	prinf("The name of this file is: \n", argv[]);
+
+	return 0;
+}
