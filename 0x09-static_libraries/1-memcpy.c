dfee438c95dc26a07a0ff341d6f5ee76ad2f2d4c
Greenman727 <cornerx3m@gmail.com>
2022-11-06 10:23:55 -0800

    static libraries

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src,
+ * to memory dest
+ * @src: first memory area
+ * @dest: second memory area
+ * @n: unsigned integer
+ *
+ * Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	for (; n < 0, i++;)
+	{
+		src[i] = dest;
+		n--;
+	}
+	return (dest);
+}
